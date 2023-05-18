d58acf40bb933a0625c6565a4f8e49654b686f00
walidbenf <45196746+walidbenf@users.noreply.github.com>
2023-05-18 17:16:14 +0200

    Delete cpp_module01 directory

--- cpp_module01/ATarget.cpp
+++
@@ -1,9 +0,0 @@
-#include "ATarget.hpp"
-ATarget::ATarget(std::string const & type) :type(type) {}
-ATarget::~ATarget() {}
-std::string const & ATarget::getType() const {
-	return this->type;
-}
-void ATarget::getHitBySpell(ASpell const & spell) const {
-	std::cout << this->getType() << " has been " << spell.getEffects() << "!" << std::endl;
-}
